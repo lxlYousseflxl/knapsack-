a21847dcdb24c0ecac7e77e967f5d380fb59f4ed
Omar-109 <70961596+Omar-109@users.noreply.github.com>
2021-02-02 19:10:36 +0200

    Update function2.cpp

--- function2.cpp
+++ function2.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include "function2.h"
 using namespace std;
+int knapSack(int W, int wt[], int val[], int n)
+{
+  int knapSack(int W, int wt[], int val[], int n)
+    return 0;
+  if (wt[n - 1] > W)
+return knapSack(W, wt, val, n - 1);
+  else
+return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt,val, n - 1));
+}
